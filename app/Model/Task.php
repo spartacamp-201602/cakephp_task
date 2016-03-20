421480f21cc3d026077160f990ba5f9b4a8cd6e7
vagrant <ihatov08@gmail.com>
2016-03-20 12:28:55 +0900

    validationの実装

--- app/Model/Task.php
+++ app/Model/Task.php
@@ -2,5 +2,35 @@
 
 class Task extends AppModel
 {
-
+    //この中にバリデーションを書く
+    public $validate = array(
+        //ここにルールを書いていく
+        // 1) 対象となるデータは？
+        // 2) どんなバリデーションをするか？
+        // 3) 引っかかったらどんなメッセージをだすか？
+        'name' => array(
+            'rule' => array('between', 5, 30),
+                'message' => 'タスクは5文字以上30文字以内で入力してください'
+            ),
+        'body' => array(
+            'rule' => array('maxLength', 255),
+            'message' => '詳細は255文字以内で入力してください'
+            )
+        );
 }
+
+
+// 今までのバリデーション
+
+// $name = $_POST['name'];
+// $errors = array();
+
+// if ($name = "")
+// {
+//     $errors[] = "名前が入力されていません"
+// }
+
+// foreach ($errors as $error)
+// {
+//     echo $error
+// }
