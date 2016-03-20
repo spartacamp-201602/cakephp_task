b333c8c41da80a46afd35c41d48e89dec936c040
vagrant <ihatov08@gmail.com>
2016-03-20 09:36:06 +0900

    タスク完了機能の実装

--- app/Controller/TasksController.php
+++ app/Controller/TasksController.php
@@ -6,6 +6,8 @@ class TasksController extends AppController
 
     public $helpers = array('Html');
 
+    public $components = array('Flash');
+
     public function index()
     {
         $options = array('conditions' => array('Task.status' => 0));
@@ -14,4 +16,19 @@ class TasksController extends AppController
 
         $this->set('tasks', $tasks);
     }
+
+    public function done($id)
+    {
+        //URLの末尾からタスクのIDを取得してデータを更新
+        $this->Task->id = $id;
+        $this->Task->saveField('status', 1);
+
+        $msg = sprintf('タスク %s を完了しました', $id);
+
+        //フラッシュメッセージ
+        $this->Flash->success($msg);
+
+        return $this->redirect(array('action' => 'index'));
+    }
+
 }
