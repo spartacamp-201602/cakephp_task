8afd3348628e6e866336dfe50539806f3bec9a89
vagrant <ihatov08@gmail.com>
2016-03-20 09:29:55 +0900

    indexページの作成

--- app/Controller/TasksController.php
+++ app/Controller/TasksController.php
@@ -2,5 +2,16 @@
 
 class TasksController extends AppController
 {
-    public $scaffold;
+    // public $scaffold;
+
+    public $helpers = array('Html');
+
+    public function index()
+    {
+        $options = array('conditions' => array('Task.status' => 0));
+
+        $tasks = $this->Task->find('all', $options);
+
+        $this->set('tasks', $tasks);
+    }
 }
